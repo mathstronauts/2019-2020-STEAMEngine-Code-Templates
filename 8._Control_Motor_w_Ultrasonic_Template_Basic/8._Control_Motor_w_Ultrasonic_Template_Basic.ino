737c09bcac893c85fd49ece038851bfa5fa5192b
Richard Hamilton <hamiltra@mcmaster.ca>
2019-10-28 22:57:12 -0400

    Added template for basic program - Richard Hamilton

---
+++ 8._Control_Motor_w_Ultrasonic_Template_Basic/8._Control_Motor_w_Ultrasonic_Template_Basic.ino
@@ -0,0 +1,25 @@
+#include <mathstronauts_arduino_library.h>
+int motor =13;
+int trig =8;
+int echo =9;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(motor, OUTPUT);
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+}
+
+void loop() {
+  //1. Read distance from ultrasonic
+  float distance = ultrasonicDistance();
+  //2. Check if distance is less than x
+  if(){
+    //Turn on motor
+    digitalWrite();
+  }
+  else{
+    // Turn off motor 
+    digitalWrite();
+  }
+  delay(100);
+}
